5069335534b13fd72a572250b6d7b2a79322df57
Saraswati Tiwari <saraswati99Tiwari@gmail.com>
2026-01-31 11:37:44 +0530

    session

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,10 +1,17 @@
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.pool import NullPool
 
 from app.config.config import config
 
-db_engine = create_async_engine(config.RDS_URI, pool_pre_ping=True, echo=False)
+# db_engine = create_async_engine(config.RDS_URI, pool_pre_ping=True, echo=False)
+db_engine = create_async_engine(
+    config.RDS_URI,
+    poolclass=NullPool,   # ← this disables SQLAlchemy pooling
+    pool_pre_ping=True,
+    echo=False
+)
 async_session_maker = async_sessionmaker(
     bind=db_engine,
     class_=AsyncSession,
