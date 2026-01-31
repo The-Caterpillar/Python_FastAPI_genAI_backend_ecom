eba1bfc2d05c10d7ede508ad6546f6e486875324
Saraswati Tiwari <saraswati99Tiwari@gmail.com>
2026-01-31 17:34:05 +0530

    back to old session

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,25 +1,10 @@
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.pool import NullPool
 
 from app.config.config import config
 
-db_engine = create_async_engine(
-    config.RDS_URI,
-    print("USING DATABASE:", config.RDS_URI),
-    pool_size=1,
-    max_overflow=0,
-    pool_timeout=60,
-    pool_pre_ping=True,
-    echo=False,
-    connect_args={
-        "timeout": 60,
-        "command_timeout": 60,
-        "ssl": "require",
-    },
-)
-
+db_engine = create_async_engine(config.RDS_URI, pool_pre_ping=True, echo=False)
 async_session_maker = async_sessionmaker(
     bind=db_engine,
     class_=AsyncSession,
@@ -30,8 +15,11 @@ async def acquire_db_session() -> AsyncGenerator[AsyncSession, None]:
     session = async_session_maker()
     try:
         yield session
+        await session.commit()
     except Exception as ex:
         await session.rollback()
         raise ex
     finally:
         await session.close()
+ 
+ 
